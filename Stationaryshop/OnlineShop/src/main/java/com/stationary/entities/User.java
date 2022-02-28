cfa01018f3d769a1e5221345837a4606ad400f6b
vivek201102 <vivekpatel201102@gmail.com>
2022-02-28 23:56:08 +0530

    Sign Up added

--- Stationaryshop/OnlineShop/src/main/java/com/stationary/entities/User.java
+++ Stationaryshop/OnlineShop/src/main/java/com/stationary/entities/User.java
@@ -1,8 +1,16 @@
 package com.stationary.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
 	private String cid;
 	private String name;
 	private String password;
